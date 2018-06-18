7a0f499179948227ec1a91e13ec95f82bd2cc1be
Vincent Hua <vincenthua@Vincents-MBP.wireless.yale.internal>
2018-06-17 23:42:59 -0400

    logging

--- routes/home.js
+++ routes/home.js
@@ -1,35 +1,35 @@
-var express = require('express');
-var router = express.Router();
-var config = require('../bin/config');
-var auth = require('../bin/auth')
-var queries = require('../models/queries')
+const express = require('express');
+const router = express.Router();
+const config = require('../bin/config');
+const auth = require('../bin/auth')
+const queries = require('../models/queries')
 
 router.use('/', function (req, res, next) {
 
-  var courseID = auth.provider.body.custom_canvas_course_id;
-  var userID = auth.provider.body.custom_canvas_user_id;
+  const courseID = auth.provider.body.custom_canvas_course_id;
+  const userID = auth.provider.body.custom_canvas_user_id;
 
   if (courseID == 10184) {
     courseID = 38082;
   }
 
-  console.log('User ID: ');
-  console.log(userID);
+  console.log('User ID: ', userID);
 
-  console.log('Course ID: ');
-  console.log(courseID);
+  console.log('Course ID: ', courseID);
 
   if (auth.provider.admin) {
     if (req.query.masquerade) {
       queries.homepageQuery(parseInt(req.query.masquerade),
         courseID,
-        (module_progress,
+        (
+          module_progress,
           score,
           awarded_badge_ids,
           leaderboard,
           my_team,
           home_updates,
-          home_vids) => {
+          home_vids
+        ) => {
           console.log("______home.js module_progress :", module_progress, "______")
           res.render('home', {
             title: 'Home | ONEXYS',
@@ -46,43 +46,64 @@ router.use('/', function (req, res, next) {
           });
         });
     } else {
-      queries.homepageAdminQuery(courseID, function (module_progress, leaderboard, home_updates, home_vids, students) {
-        res.render('home', {
-          title: 'Home | ONEXYS',
-          courseID: courseID,
-          module_progress: module_progress,
-          score: 0,
-          awarded_badge_ids: [],
-          leaderboard: leaderboard,
-          my_team: {
-            Name: "Admin",
-            Score: 0
-          },
-          home_updates: home_updates,
-          home_vids: home_vids,
-          admin: auth.provider.admin,
-          masquerade: false,
-          students: students
+      queries.homepageAdminQuery(
+        courseID,
+        (
+          module_progress,
+          leaderboard,
+          home_updates,
+          home_vids,
+          students
+        ) => {
+          console.log("______home.js module_progress :", module_progress, "______")
+          res.render('home', {
+            title: 'Home | ONEXYS',
+            courseID,
+            module_progress,
+            score: 0,
+            awarded_badge_ids: [],
+            leaderboard,
+            my_team: {
+              Name: "Admin",
+              Score: 0
+            },
+            home_updates,
+            home_vids,
+            admin: auth.provider.admin,
+            masquerade: false,
+            students
+          });
         });
-      });
     }
   }
 
   else {
-    queries.homepageQuery(userID, courseID, function (module_progress, score, awarded_badge_ids, leaderboard, my_team, home_updates, home_vids) {
-      res.render('home', {
-        title: 'Home | ONEXYS',
-        courseID: courseID,
-        module_progress: module_progress,
-        score: score,
-        awarded_badge_ids: awarded_badge_ids,
-        leaderboard: leaderboard,
-        my_team: my_team,
-        home_updates: home_updates,
-        home_vids: home_vids,
-        admin: auth.provider.admin
+    queries.homepageQuery(
+      userID,
+      courseID,
+      (
+        module_progress,
+        score,
+        awarded_badge_ids,
+        leaderboard,
+        my_team,
+        home_updates,
+        home_vids
+      ) => {
+        console.log("______home.js module_progress :", module_progress, "______")
+        res.render('home', {
+          title: 'Home | ONEXYS',
+          courseID,
+          module_progress,
+          score,
+          awarded_badge_ids,
+          leaderboard,
+          my_team,
+          home_updates,
+          home_vids,
+          admin: auth.provider.admin
+        });
       });
-    });
   }
 });
 
