5b77031f443be4116df1ad57716760de6de5aaa5
Surya Dutta <suryabrata.dutta@yale.edu>
2018-06-29 15:06:57 -0400

    fix home user course id

--- routes/home.js
+++ routes/home.js
@@ -6,12 +6,16 @@ var queries = require('../models/queries')
 
 router.use('/', (req, res, next) => {
 
-  var courseID = auth.provider.custom_canvas_course_id;
-  var userID = auth.provider.custom_canvas_user_id;
+  var courseID = auth.provider.body.custom_canvas_course_id;
+  var userID = auth.provider.body.custom_canvas_user_id;
 
   console.log('User ID: ', userID, '  Course ID: ', courseID);
+  console.log(auth.provider.body);
 
   var is_physics = Boolean(courseID == 38083);
+
+  console.log('Instructor:')
+  console.log(auth.provider.body.roles.includes('Instructor'))
 
   if (courseID == 10184) {
     courseID = 38082;
