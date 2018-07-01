4cf8fba1ed19c2d0d29b94c4328fbb8ea79e7b5a
Surya Dutta <suryabrata.dutta@yale.edu>
2018-07-01 17:13:41 -0400

    fix update url

--- models/canvas.js
+++ models/canvas.js
@@ -15,7 +15,7 @@ var notes_column_url = (courseID) => {
 var get_update_url = (courseID, callback) => {    
   getAdminRequest(notes_column_url(courseID),function(err,custom_columns){
     var points_id = custom_columns.find(column => column.title='Notes').id;
-    var update_url = config.canvasURL + '/api/v1/courses/' + courseID + '/custom_gradebook_columns/' + points_id + '/data/';
+    var update_url = config.canvasURL + '/api/v1/courses/' + courseID + '/custom_gradebook_columns/' + points_id + '/data?per_page=100';
     callback(update_url);
   });
 }
