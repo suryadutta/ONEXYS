ec1a39f228ae5a884a200cc59d78862018c64cb2
Vincent Hua <vincenthua@Vincents-MBP.wireless.yale.internal>
2018-07-15 20:19:59 -0400

    hard code grading scheme for physics track last four modules

--- models/canvas.js
+++ models/canvas.js
@@ -473,28 +473,33 @@ function getStudentProgress(studentID, courseID, callback) { // Get student prog
         //get quiz and aleks progress
         for (var i = 0; i < moduleProgress.length; i++) {
           var module_object = mongo_data.modules.find(module => module._id == i + 1);
+
           if ((studentID = 4671)) {
             module_object.open = 'true';
-            if (module_object._id == 7){
-              module_object.practice_link = 73624
-            }
-            if (module_object._id == 8){
-              module_object.practice_link = 73627
-            }
-            console.log("module_object", module_object);
-            var practice_object = user_assigments.find(
-              assignment =>
-                assignment.assignment_id == module_object.practice_link
-            );
-            console.log(" practice_object : ", practice_object);
           }
+
           if(module_object.open=='true'){
             //practice progress
-            var practice_object = user_assigments.find(assignment => assignment.assignment_id == module_object.practice_link);
-            if(practice_object){
-              (moduleProgress[i]).practice_progress = parseFloat(practice_object.grade) >= parseFloat(module_object.practice_cutoff);
+
+            if (!module_object.multiple_practices) {
+              var practice_object = user_assigments.find(assignment => assignment.assignment_id == module_object.practice_link);
+              if(practice_object){
+                (moduleProgress[i]).practice_progress = parseFloat(practice_object.grade) >= parseFloat(module_object.practice_cutoff);
+              } else {
+                (moduleProgress[i]).practice_progress = false;
+              }
             } else {
-              (moduleProgress[i]).practice_progress = false;
+              const practice_objects = module_object.multiple_practice_links.map(link_id => user_assignments.find(assignment => assignment.assignment_id == link_id));
+              if(practice_objects
+              .every(practice_object => parseFloat(practice_object.grade) >= parseFloat(module_object.practice_cutoff))){
+                (moduleProgress[i]).practice_progress = true;
+              } else {
+                (moduleProgress[i]).practice_progress = false;
+              }
+              if ((studentID = 4671)) {
+                console.log("module:", module_object);
+                console.log("practice_objects:", practice_objects);
+              }
             }
 
             //quiz progress
