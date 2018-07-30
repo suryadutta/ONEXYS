0e666d94231f32925342bfbc3dc6f2ea5e95776d
Vincent Hua <vincenthua@Vincents-MacBook-Pro.local>
2018-07-30 02:49:56 -0400

    add: edit multiple cutoffs

--- models/canvas.js
+++ models/canvas.js
@@ -484,12 +484,12 @@ function getStudentProgress(studentID, courseID, callback) { // Get student prog
               
             const practice_objects = Object.keys(practiceId_cutoff_obj).map(practice_id => user_assigments.find(assignment => assignment.assignment_id == parseInt(practice_id)));
             
-            // if(practice_objects
-            //   .every(practice_object => parseFloat(practice_object.grade) >= parseFloat(practiceId_cutoff_obj[practice_object]))){
-            //     (moduleProgress[i]).practice_progress = true;
-            //   } else {
-            //     (moduleProgress[i]).practice_progress = false;
-            //   }
+            if(practice_objects
+              .every(practice_object => parseFloat(practice_object.grade) >= parseFloat(practiceId_cutoff_obj[practice_object.assignment_id + '']))){
+                (moduleProgress[i]).practice_progress = true;
+              } else {
+                (moduleProgress[i]).practice_progress = false;
+              }
 
           } else{
             if (!module_object.multiple_practices) {
