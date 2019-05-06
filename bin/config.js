a57b56c0b621468733db74354d84ab7f31629f97
walkianm <walkianm@gmail.com>
2019-05-06 14:31:26 -0400

    Changed mongo connection string to new mongodb

--- bin/config.js
+++ bin/config.js
@@ -20,12 +20,12 @@ config.local_mongo_database = 'mongodb://localhost:27017/oneyxs_dev'
 config.disableLeaderboard = process.env.DISABLE_LEADERBOARD.toLowerCase() == 'true' ? true : false || false;
 
 //configure MongoDB urls here - course ID to mongoDB URL
-config.mongoURL = 'mongodb://'+config.mongo_username+':'+config.mongo_password+'@onexys-shard-00-00-qkreu.mongodb.net:27017,onexys-shard-00-01-qkreu.mongodb.net:27017,onexys-shard-00-02-qkreu.mongodb.net:27017/uva-onexys?ssl=true&replicaSet=ONEXYS-shard-0&authSource=admin&retryWrites=true';
+config.mongoURL = 'mongodb://'+config.mongo_username+':'+config.mongo_password+'@conexcluster-shard-00-00-bgeoe.mongodb.net:27017,conexcluster-shard-00-01-bgeoe.mongodb.net:27017,conexcluster-shard-00-02-bgeoe.mongodb.net:27017/conex?ssl=true&replicaSet=ONEXYS-shard-0&authSource=admin&retryWrites=true';
 
 //add to list if we create new courses
 //format is courseID: databaseName
 config.mongoDBs = {
-  '2517': 'uva-onexys',
+  '2517': 'conex',
 }
 
 //the name of the mongo db to connect to
