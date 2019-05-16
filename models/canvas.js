96e6424e8cc1dfe66484ade68323cd319739dcbe
walkianm <walkianm@gmail.com>
2019-05-16 17:19:08 -0400

    Added logging to canvas.js

--- models/canvas.js
+++ models/canvas.js
@@ -482,6 +482,11 @@ function getStudentProgress(studentID, courseID, callback) { // Get student prog
 
           const practice_objects = Object.keys(practiceId_cutoff_obj).map(practice_id => user_assigments.find(assignment => assignment.assignment_id == parseInt(practice_id)));
 
+          console.log(practice_objects.length);
+          practiceObjects.forEach(function(element){
+            console.log(element.grade);
+          });
+
           if(practice_objects.length == 0 || practice_objects.every(practice_object => parseFloat(practice_object.grade) >= parseFloat(practiceId_cutoff_obj[practice_object.assignment_id + '']))){
             (moduleProgress[i]).practice_progress = true;
           } else {
