dbf2b482e7d7e50efb172cb674f30b311751c58d
gmf5de <gmf5de@virginia.edu>
2019-05-17 14:29:33 -0400

    added documentation

--- public/javascript/fade.js
+++ public/javascript/fade.js
@@ -1,15 +1,12 @@
 // Used to swap out forms in edit home page section of the admin panel
 
-
-// Switches the form into single update mode. In this mode, a single
-// update name and body text is available to enter, and the other
-// fields are filled out automatically by the system to propagate
-// existing updates downwards.
-
-// Store initially provided information for context (mode) switching later on
+// Variables to store initially provided information for context
+// (mode) switching later on.
 var pHead1, pHead2, pHead3;
 var pText1, pText2, pText3;
 
+// When the document loads, populate the variables with the current
+// information.
 $(document).ready(function() {
     pHead1 = $('#main_header').attr('value');
     pText1 = $('#main_text').text();
@@ -22,8 +19,13 @@ $(document).ready(function() {
 });
 
 
-
+// Switches the form into single update mode. In this mode, a single
+// update name and body text is available to enter, and the other
+// fields are filled out automatically by the system to propagate
+// existing updates downwards.
 function doSingleUpdate() {
+    // Clear out the first pair of fields (so the first entry is
+    // blank), then shift the existing text fields down one.
     $('#main_header').attr('value', '');
     $('#main_text').text('');
     $('#header2').attr('value', pHead1);
@@ -31,11 +33,13 @@ function doSingleUpdate() {
     $('#header3').attr('value', pHead2);
     $('#text3').text(pText2);
 
+    // Hide the fields for the second and third updates
     $('#header2').hide();
     $('#text2').hide();
     $('#header3').hide();
     $('#text3').hide();
 
+    // Also hide their labels
     $('#lbl0').hide();
     $('#lbl1').hide();
     $('#lbl2').hide();
@@ -45,6 +49,7 @@ function doSingleUpdate() {
 // Switches the form into edit existing updates mode. In this mode,
 // the three currently visible updates are all available to edit.
 function doExistingUpdates() {
+    // Restore the fields to the original provided data
     $('#main_header').attr('value', pHead1);
     $('#main_text').text(pText1);
     $('#header2').attr('value', pHead2);
@@ -52,11 +57,13 @@ function doExistingUpdates() {
     $('#header3').attr('value', pHead3);
     $('#text3').text(pText3);
 
+    // Fade in the other 2 update fields
     $('#header2').fadeIn('slow');
     $('#text2').fadeIn('slow');
     $('#header3').fadeIn('slow');
     $('#text3').fadeIn('slow');
 
+    // Also fade in their labels
     $('#lbl0').fadeIn('slow');
     $('#lbl1').fadeIn('slow');
     $('#lbl2').fadeIn('slow');
