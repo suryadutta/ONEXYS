cf3d98015c2704258b7e34ced6281213375bda35
walkianm <walkianm@gmail.com>
2019-05-24 19:15:25 -0400

    Enabled leaderboard

--- dependencies.py
+++ dependencies.py
@@ -39,5 +39,5 @@ for f in get_file_list(os.getcwd()):
 
     with open(f, 'r', encoding='latin-1') as contents:
         contents = contents.read()
-        if 'multiple_practice_cutoff' in contents:
+        if 'getSections' in contents:
             print(f)
