d44a1267428f1cb037c1c8a48555a5e80727e3b4
walkianm <walkianm@gmail.com>
2019-05-27 19:40:10 -0400

    Added logging

--- public/javascript/badges.js
+++ public/javascript/badges.js
@@ -9,8 +9,10 @@ function setEarnedBadges(){
     $(".badge_container").each(function(badge){
         var portrait = $(badge).children(".badge_portrait");
         if($(badge).hasClass("completed")){
+            console.log('url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("earned_url") + ')');
             $(portrait).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("earned_url") + ')');
         }else{
+            console.log('url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("unearned_url") + ')');
             $(portrait).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("unearned_url") + ')');
         }
     });
@@ -19,9 +21,11 @@ function setEarnedBadges(){
 $(".completed .badge_portrait").hover(
     function(){
         // mouse in
+        console.log("over!");
         $(this).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(this).attr("earned_hover_url") + ')');
     }, function(){
         // mouse out
+        console.log("out!");
         $(this).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(this).attr("earned_url") + ')');
     }
 );
