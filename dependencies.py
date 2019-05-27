5e75efabb0b6bc6da680b79ccef7052366d1aede
walkianm <walkianm@gmail.com>
2019-05-27 19:26:21 -0400

    Removed onexys_badge class

--- dependencies.py
+++ dependencies.py
@@ -39,5 +39,5 @@ for f in get_file_list(os.getcwd()):
 
     with open(f, 'r', encoding='latin-1') as contents:
         contents = contents.read()
-        if 'images/badge' in contents:
+        if 'onexys_badge' in contents:
             print(f)
--- public/javascript/badges.js
+++ public/javascript/badges.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 // for each badge image, sets to completed if the badge has been earned
 function setEarnedBadges(){
-    $("#badge_display").children().forEach(function(badge){
+    $(".badge_container").forEach(function(badge){
         var portrait = badge.children(".badge_portrait");
         if(badge.hasClass("completed")){
             portrait.css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + portrait.attr("earned_url") + ')');
