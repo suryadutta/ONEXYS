1ea1e712a2c87af5f60e7369bfbec8f7a3a9cbbb
walkianm <walkianm@gmail.com>
2019-05-27 19:15:56 -0400

    Reworked new badges structure

--- public/javascript/badges.js
+++ public/javascript/badges.js
@@ -7,10 +7,21 @@ $(document).ready(function() {
 // for each badge image, sets to completed if the badge has been earned
 function setEarnedBadges(){
     $("#badge_display").children().forEach(function(badge){
+        var portrait = badge.children(".badge_portrait");
         if(badge.hasClass("completed")){
-            badge.children(".badge_portrait").css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + badge.attr("earned_url") + ')');
+            portrait.css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + portrait.attr("earned_url") + ')');
         }else{
-            badge.children(".badge_portrait").css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + badge.attr("unearned_url") + ')');
+            portrait.css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + portrait.attr("unearned_url") + ')');
         }
     });
 }
+
+$(".completed .badge_portrait").hover(
+    function(){
+        // mouse in
+        $(this).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(this).attr("earned_hover_url") + ')');
+    }, function(){
+        // mouse out
+        $(this).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(this).attr("earned_url") + ')');
+    }
+);
