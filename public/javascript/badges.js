1c4954d562a2ec4b2bc0db1b0454707d0324ca64
walkianm <walkianm@gmail.com>
2019-05-27 19:33:53 -0400

    Fixed jQuery targets

--- public/javascript/badges.js
+++ public/javascript/badges.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
 // for each badge image, sets to completed if the badge has been earned
 function setEarnedBadges(){
     $(".badge_container").each(function(badge){
-        var portrait = badge.children(".badge_portrait");
-        if(badge.hasClass("completed")){
-            portrait.css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + portrait.attr("earned_url") + ')');
+        var portrait = $(badge).children(".badge_portrait");
+        if($(badge).hasClass("completed")){
+            $(portrait).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("earned_url") + ')');
         }else{
-            portrait.css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + portrait.attr("unearned_url") + ')');
+            $(portrait).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("unearned_url") + ')');
         }
     });
 }
