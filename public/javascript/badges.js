624c70c1e02ab6f233ddf974eae45e5cca639d51
walkianm <walkianm@gmail.com>
2019-05-27 20:09:49 -0400

    JQuery fix

--- public/javascript/badges.js
+++ public/javascript/badges.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
 
 // for each badge image, sets to completed if the badge has been earned
 function setEarnedBadges(){
-    $(".badge_container").each(function(badge){
-        var portrait = $(badge).children(".badge_portrait")[0];
-        if($(badge).hasClass("completed")){
-            console.log('url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("earned_url") + ')');
+    $(".badge_container").each(function(){
+        var portrait = $(this).children(".badge_portrait");
+        if($(this).hasClass("completed")){
+            console.log($(portrait).attr("earned_url"));
             $(portrait).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("earned_url") + ')');
         }else{
-            console.log('url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("unearned_url") + ')');
+            console.log($(portrait).attr("unearned_url"));
             $(portrait).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("unearned_url") + ')');
         }
     });
