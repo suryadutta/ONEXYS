e0ee994cdb14534e4b3fd4d5f6afa6aa7cc186c1
walkianm <walkianm@gmail.com>
2019-05-27 21:28:15 -0400

    Replaced some JS with CSS

--- public/javascript/badges.js
+++ public/javascript/badges.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function() {
-    setEarnedBadges();
 
     $(".completed .badge_portrait").hover(
         function(event){
@@ -12,16 +11,3 @@ $(document).ready(function() {
         }
     );
 });
-
-
-// for each badge image, sets to completed if the badge has been earned
-function setEarnedBadges(){
-    $(".badge_container").each(function(){
-        var portrait = $(this).children(".badge_portrait");
-        if($(this).hasClass("completed")){
-            $(portrait).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("earned_url") + ')');
-        }else{
-            $(portrait).css('background-image', 'url(https://conex.herokuapp.com/images/badgePics/' + $(portrait).attr("unearned_url") + ')');
-        }
-    });
-}
