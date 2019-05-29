b9bfa09a7d56fe00f4f470470d35f4e7c3586fc5
walkianm <walkianm@gmail.com>
2019-05-29 18:08:15 -0400

    Added homepage updates to POST

--- routes/admin.js
+++ routes/admin.js
@@ -160,9 +160,9 @@ router.get("/modules", (req, res, next) => {
 });
 
 router.post("/modules", (req, res, next) => {
-    console.log("Module POST");
-    console.log(req.body);
-    console.log(req.body.post_test);
+  mongo.updateData(req.session.course_id, "home", { type: "updates" }, req.body, (err, result) => {
+    res.redirect("admin/modules");
+  });
 });
 
 //Get Page to Edit Module Content
