4872d4931ffcefcb1ee1d4cb5fca921ce7263aef
gmf5de <gmf5de@virginia.edu>
2019-06-01 15:10:36 -0400

    javascript is not python

--- routes/admin.js
+++ routes/admin.js
@@ -402,8 +402,8 @@ router.get("/dailies/edit/:id", (req, res, next) => {
 
 router.post("/dailies/edit/:id", (req, res, next) => {
     console.log("POSTing");
-    var valid_assignment_ids[];
-    var valid_quiz_ids[];
+    var valid_assignment_ids = [];
+    var valid_quiz_ids = [];
     canvas.getAdminRequest(req.session.course_id, function(err, assignment_list) {
         console.log("Entered admin request");
         assignment_list.forEach(function(assignment) {
