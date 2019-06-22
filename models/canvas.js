b5cad674cdf81d1f09af6231a2515f089e22e432
G.Michael Fitzgerald <gmf5de@virginia.edu>
2019-06-21 22:12:27 -0400

    provide correct students reference

--- models/canvas.js
+++ models/canvas.js
@@ -594,7 +594,7 @@ function getLeaderboardScores(studentID, courseID, course_title, callback) { // 
                 groupNames = data.map(section => section.name);
                 studentIdsArrays = data.map((section) => {
                     console.log('Found a section');
-                    if(students) return section.students.map(studentInfo => studentInfo.id);
+                    if(section.students) return section.students.map(studentInfo => studentInfo.id);
                     else {
                         console.log('Encountered section with no students. Inserting empty array placeholder');
                         return [];
