137d3de74f4416bbcd61981e7126773ec46d60c9
G.Michael Fitzgerald <gmf5de@virginia.edu>
2019-06-23 18:41:59 -0400

    updated life on grounds preview page for generality

--- routes/admin.js
+++ routes/admin.js
@@ -130,6 +130,8 @@ router.post('/home/videos/preview/:id', (req,res,next) => {
     user_id: req.session.user_id,
     video: req.body,
     video_id: req.params.id,
+    life_on_grounds_title: req.life_on_grounds_title,
+    life_on_grounds_thumbnail: req.life_on_grounds_thumbnail,
     heroku_app: config.herokuAppName
   });
 });
