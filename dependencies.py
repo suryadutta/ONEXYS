aad6f7cccfd0d0a9505da60de9a2f2f0eb95193f
walkianm <walkianm@gmail.com>
2019-07-04 16:21:54 -0400

    Temporary restore

--- dependencies.py
+++ dependencies.py
@@ -39,5 +39,5 @@ for f in get_file_list(os.getcwd()):
 
     with open(f, 'r', encoding='latin-1') as contents:
         contents = contents.read()
-        if 'Do you really' in contents:
+        if 'Here' in contents:
             print(f)
