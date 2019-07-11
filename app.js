49aca0a44f35759bbd8a7d23b1a1f82c62f5d3b6
walkianm <walkianm@gmail.com>
2019-07-10 20:44:06 -0400

    Removed error handler

--- app.js
+++ app.js
@@ -129,11 +129,13 @@ app.post("/not-open", function(req, res) {
 });
 
 // catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+*/
 
 // error handler
 app.use(function(err, req, res, next) {
