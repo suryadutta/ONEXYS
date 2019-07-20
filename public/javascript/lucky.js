5147d71093a2f9d695b6cf8a681b712e67c899f8
walkianm <walkianm@gmail.com>
2019-07-20 12:59:59 -0400

    Jquery change

--- public/javascript/lucky.js
+++ public/javascript/lucky.js
@@ -2,10 +2,8 @@
 
 $(document).ready(function() {
     $(".full_background").removeClass('hide');
-    alert("Fade in!");
 });
 
-$(".full_background").addEventListener('click', function(){
+$(".full_background").click(function(){
     $(".full_background").addClass('hide');
-    alert("Fade out!");
 });
