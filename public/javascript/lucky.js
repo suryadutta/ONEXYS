d79801b1baf069fdd45950f60b951029045a8b7d
walkianm <walkianm@gmail.com>
2019-07-20 13:13:14 -0400

    More logs

--- public/javascript/lucky.js
+++ public/javascript/lucky.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
     $(".full_background").removeClass('hide');
 });
 
+$("*").click(function(else){
+    console.log(e);
+});
+
 $(".full_background").click(function(){
     console.log("Hide!");
     $(".full_background").addClass('hide');
