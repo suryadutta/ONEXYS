3b1d30e25987c97bcf5e38896dbf76333d5cf128
walkianm <walkianm@gmail.com>
2019-07-20 14:54:38 -0400

    Force fade out

--- public/javascript/lucky.js
+++ public/javascript/lucky.js
@@ -1,11 +1,12 @@
 // Manages the display of luckies!
 
 $(document).ready(function() {
-    $(".full_background").fadeIn(500);
-
     // Hide on click
     $(".full_background").click(function(e){
         console.log("Hide!");
         $(".full_background").fadeOut(500);
     });
+
+    // Show for max of 7 seconds
+    $(".full_background").fadeIn(1000).delay(7000).fadeOut(500);
 });
