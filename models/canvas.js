35c15c7799f410e7e1ac8151cc4b904af9bce7bf
walkianm <walkianm@gmail.com>
2019-07-24 18:26:44 -0400

    Fixed login in lucky granting

--- models/canvas.js
+++ models/canvas.js
@@ -1293,13 +1293,11 @@ var awardLuckies = function(req, res, next) {
         console.log(luckies);
         if (luckies.length>0){
             for (lucky_bulldog of luckies){
-                if(lucky_bulldog.awarded_ids.length>0){
-                    // If student not already on list AND within a minute of the assigned time...
-                    if (!lucky_bulldog.awarded_ids.includes(studentID) && ((d.getTime() - Date.parse(lucky_bulldog.time))/(1000*60))<1){
-                        lucky_bulldog.awarded_ids.push(studentID);
-                        mongo.updateData(req.session.course_id,'lucky_bulldogs',{ _id: parseInt(lucky_bulldog._id) },{awarded_ids: lucky_bulldog.awarded_ids}, function(err,result){});
-                        req.session.lucky = lucky_bulldog;
-                    }
+                // If student not already on list AND within a minute of the assigned time...
+                if (!lucky_bulldog.awarded_ids.includes(studentID) && ((d.getTime() - Date.parse(lucky_bulldog.time))/(1000*60))<1){
+                    lucky_bulldog.awarded_ids.push(studentID);
+                    mongo.updateData(req.session.course_id,'lucky_bulldogs',{ _id: parseInt(lucky_bulldog._id) },{awarded_ids: lucky_bulldog.awarded_ids}, function(err,result){});
+                    req.session.lucky = lucky_bulldog;
                 }
             }
         }
