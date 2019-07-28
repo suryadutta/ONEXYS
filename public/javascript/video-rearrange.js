b9b6852e3e05adc7dd98b6083b0f9dbb49ad28d0
gmf5de <gmf5de@virginia.edu>
2019-07-27 23:30:59 -0400

    Run error check on completion

--- public/javascript/video-rearrange.js
+++ public/javascript/video-rearrange.js
@@ -6,14 +6,18 @@ $(document).ready(function() {
             revert: true, // Have items glide back into place if not dropped perfectly
             update: (event, ui) => { // Whenever the order is changed, post changes via AJAX
                 if(heroku) {
+                    var end = $("#home-videos-container").children().length - 1, failed = "";
                     $("#home-videos-container").children().each(function(i) { // Make AJAX req to update each video
                         $.get("https://" + heroku + ".herokuapp.com/admin/updateVideo", {
                             id: $(this).attr("id"),
                             position: i,
-                        }, (data, status) => {
-                            if(status != 'success') alert("Rearrangement of videos failed due to a server error. Try again later.");
+                            complete: (data, status) => {
+                                if(status != 'success') failed += $(this).attr('id') + " ";
+                                if(i == end && failed.length > 0) alert("Rearrangement of videos " + failed + "failed due to a server error. Try again later.");
+                            }
                         });
                     });
+
                 } else alert("You're changes are not being saved because the local pointer to heroku was not set by the server or has been modified.");
             }
         });
