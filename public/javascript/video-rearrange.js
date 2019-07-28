61ce5e0a1cbfd96a4022cb9d60c452656ffe7e7a
gmf5de <gmf5de@virginia.edu>
2019-07-27 21:45:08 -0400

    Create updateVideo route for AJAX; add some safeguards; log many things

--- public/javascript/video-rearrange.js
+++ public/javascript/video-rearrange.js
@@ -1,15 +1,26 @@
 
 $(document).ready(function() {
-    // Initialize jQuery UI 'sortability' (allows rearragement of items)
-	$("#home-videos-container").sortable({
-		revert: true, // Have items glide back into place if not dropped perfectly
-		update: (event, ui) => { // Whenever the order is changed, post changes via AJAX
-			$("#home-videos-container").children().each(function() {
-				console.log($(this).attr("id"));
-			});
-		}
-	});
+    console.log("https://" + heroku + ".herokuapp.com/updateVideo");
+    if(heroku) { // If the server was unable to serve this, we cannot submit AJAXes, so don't even make them movable
+        $("#home-videos-container").sortable({ // Initialize jQuery UI 'sortability' (allows rearragement of items)
+            revert: true, // Have items glide back into place if not dropped perfectly
+            update: (event, ui) => { // Whenever the order is changed, post changes via AJAX
+                $("#home-videos-container").children().each(function(i) {
+                    if(heroku) {
+                        console.log(i);
+                        $.get("https://" + heroku + ".herokuapp.com/updateVideo", {
+                            id: $(this).attr("id"),
+                            position: i,
+                        }, (data, status) => {
+                            console.log(data);
+                            console.log(status);
+                        });
+                    } else alert("Rearrangement of videos has not been saved due to a server error. Try reloading the page.");
+                });
+            }
+        });
+    }
 
-    // Disable text highlight on items you can drag
+    // Disable text highlight on drag-able items
 	$("#home-videos-container, .onexys-video").disableSelection();
 });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var assert = require('assert');
+
+// AJAX uses this route to dynamically apply video reordering support
+router.use('updateVideo', (req, res) => {
+    try {
+        console.log(req.session);
+        //assert.notEqual(req.session.user, null);
+        assert.equals(req.method, 'GET');
+        mongo.updateData(req.session.course_id, "home", { _id: req.params.id }, parseInt(req.params.position), (err, result) => {
+            if(err) {
+                res.status()
+            } else {
+                res.status(200);
+                res.send("200 - OK");
+            }
+        });
+    } catch(e) {
+        res.status(400);
+        res.send("400 - Bad Request. The given method, syntax, or protocol is not currently supported.");
+    }
+});
 
 /* GET home page. */
 router.use('/', function(req, res, next) {
