23c6cac443271f2d151a296fca5a74051c2afd8e
Jared Nguyen <jtn2km@virginia.edu>
2021-05-21 04:45:12 -0400

    Update modules route for new course id Re-added public images folder

--- routes/modules.js
+++ routes/modules.js
@@ -1,30 +1,30 @@
 var express = require("express"),
-    router = express.Router(),
-    mongo = require("../models/mongo"),
-    config = require("../bin/config"),
-    assert = require("assert");
+  router = express.Router(),
+  mongo = require("../models/mongo"),
+  config = require("../bin/config"),
+  assert = require("assert");
 
 /* GET module page. */
 router.get("/:id", (req, res, next) => {
-    try {
-        // Validate inputs
-        assert(/\d+/.test(req.session.course_id));
-        assert(/\d+/.test(req.params.id));
-        mongo.getModule(req.session.course_id, req.params.id, (err, module) => {
-            if(module) {
-                res.render("module", {
-                    data: moduleData,
-                    course_number: req.session.course_id || "38082",
-                    canvas_url: config.canvasURL,
-                    heroku_app: config.herokuAppName,
-                    lucky: req.session.lucky
-                });
-            } else res.status(406).send("Module doesn't exist");
+  try {
+    // Validate inputs
+    assert(/\d+/.test(Object.keys(req.session.course_id)[0]));
+    assert(/\d+/.test(req.params.id));
+    mongo.getModule(Object.keys(req.session.course_id)[0], req.params.id, (err, moduleData) => {
+      if (module) {
+        res.render("module", {
+          data: moduleData,
+          course_number: req.session.course_id || "38082",
+          canvas_url: config.canvasURL,
+          heroku_app: config.herokuAppName,
+          lucky: req.session.lucky,
         });
-    } catch(e) {
-        res.status(406).send("Bad input");
-        //res.render("errors/inputError");
-    }
+      } else res.status(406).send("Module doesn't exist");
+    });
+  } catch (e) {
+    res.status(406).send("Bad input");
+    //res.render("errors/inputError");
+  }
 });
 
 module.exports = router;
