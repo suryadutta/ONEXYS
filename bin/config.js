96496913338e4676fe5a4d8828ffe9a824e70223
jimrolf <jim.rolf@yale.edu>
2021-07-05 13:30:49 -0400

    added physics21 to config.js

--- bin/config.js
+++ bin/config.js
@@ -39,7 +39,8 @@ config.mongoDBs = {
   8375: "conexWhite21",
   8374: "conexBlue21",
   8369: "smithBlue21",
-  8367: "trinityBlue21",
+  8467: "physics21",
+  
 };
 
 // If we're in a development environment, use the development databasez
