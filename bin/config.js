04beddd8af084544b0c8df7bf7bf92952e96105f
jimrolf <jim.rolf@yale.edu>
2022-05-28 14:34:29 -0400

    Update config.js

--- bin/config.js
+++ bin/config.js
@@ -61,7 +61,6 @@ config.mongoDBs = {
   '13269': "AscendWhite22",
   '13270': "AscendGreen22",
   '13322': "AscendBlueCoach22",
-  '13367': "conexBlueCoach22",
   '13369': "conexWhite22",
   '13366': "conexBlueCoach22",
   
